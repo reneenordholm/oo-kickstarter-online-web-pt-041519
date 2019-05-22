77eb752919ca5dbd71af59925d87f6b610ac8aa3
Renee Nordholm <reneenordholm@gmail.com>
2019-05-22 19:14:38 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,14 @@
+class Backer
+  attr_reader :backed_projects, :name, :title
+  
+  def initialize(name)
+    @name = name
+    @title = title
+    @backed_projects = []
+  end
+  
+  def back_project(project)
+    @backed_projects << project
+    project.backers << self
+  end
+end
