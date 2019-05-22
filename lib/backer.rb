00e53ca9f1308c12b9b088f2a08192aee0d7683f
Renee Nordholm <reneenordholm@gmail.com>
2019-05-22 18:55:21 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,7 +8,7 @@ class Backer
     @backed_projects = []
   end
   
-  def back_project(project)
-    @backed_projects << Project.new(project)
+  def back_project(Project)
+    @backed_projects << Project
   end
 end
